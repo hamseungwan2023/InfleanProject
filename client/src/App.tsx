de4a1518f57df66ca2b7fb2141bbaeab4737bc28
hamseungwan2023 <syham001@naver.com>
2023-12-28 15:05:15 +0900

    초기 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Join from './routes/join';
-import Login from './routes/login';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Join from "./routes/join";
+import Login from "./routes/login";
 
 const router = createBrowserRouter([
   {
     path: "/join",
-    element: <Join />
+    element: <Join />,
   },
   {
     path: "/login",
-    element: <Login />
+    element: <Login />,
   },
-])
+]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
