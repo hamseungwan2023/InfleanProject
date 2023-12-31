0d2569a3002792f07fa53c8f0a0d9764ec95ced2
Kyedonghwan <kdh6437@nts-corp.com>
2023-12-31 13:35:56 +0900

    회원가입 프로필 이미지 추가

--- client/src/routes/join.tsx
+++ client/src/routes/join.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import '../App.css';
 
 const Join = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
+  const [profileImg, setProfileImg] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -13,7 +15,7 @@ const Join = () => {
     setIsLoading(true);
     try{
       await axios.post("/user/signup" , {
-        username, nickname, password, email
+        username, nickname, password, email, profileImg
       }); 
 
     } catch(e) {
@@ -35,6 +37,20 @@ const Join = () => {
     }
   }
 
+  const onProfileImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if(files && files.length === 1) {
+      const file = files[0];
+      /* const locationRef = ref(storage, `avatar/${user?.uid}`);
+      const result = await uploadBytes(locationRef, file);
+      const uploadUrl = await getDownloadURL(result.ref); */
+      const uploadUrl ="업로드된 url 받으면 여기에";
+      setProfileImg(uploadUrl);
+    }else {
+      return;
+    }
+  }
+
   return (
     <form onSubmit={onSubmit}>
       <label htmlFor="username">ID</label>
@@ -45,7 +61,22 @@ const Join = () => {
       <input onChange={onChange} name="nickname" type="text" placeholder="닉네임을 입력하세요" value={nickname}/>
       <label htmlFor="email">Email</label>
       <input onChange={onChange} name="email" type="email" placeholder="이메일을 입력하세요" value={email}/>
-      <input type="submit" value={isLoading ? "제출 중 ..." : "제출하기"} />
+      <label htmlFor="profileImage" id="profileImageLabel">
+      {profileImg ? (
+          <img src={profileImg} alt="회원가입 프로필 이미지" style={{width: "100px"}}/>
+        ) : 
+        <svg
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        >
+          <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+        </svg>
+        }
+      </label>
+      <input name="profileImage" type="file" id="profileImage" accept="image/*" onChange={onProfileImageChange} />
+      <input type="submit" value={isLoading ? "처리 중 ..." : "회원가입"} />
     </form>
   )
 }
