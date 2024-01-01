a722f03ec8f4cd69000709467264d7694a24023d
Kyedonghwan <kdh643737@gmail.com>
2024-01-01 17:25:50 +0900

    footer 구현

--- client/src/routes/Layout.tsx
+++ client/src/routes/Layout.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 
 const Layout = () => {
   return <>
     <Header />
+    <Outlet />
+    <Footer />
   </>
 }
 
